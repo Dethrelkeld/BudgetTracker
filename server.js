b18ae97b169efbbce72ddccbce0ef5fa8948ab35
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-03-10 22:52:28 -0600

    deployment

--- server.js
+++ server.js
@@ -16,10 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.mongodb_uri || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
